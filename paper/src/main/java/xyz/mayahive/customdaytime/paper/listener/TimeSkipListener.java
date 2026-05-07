8cbf9b56feb15d1c272e21bcdaa3f7bc15d19f2f
seedim <seedim@mayahive.xyz>
2026-05-07 17:07:33 +0300

    fix(listener): use ClockTimeSkipEvent.SkipReason for night skip check

--- paper/src/main/java/xyz/mayahive/customdaytime/paper/listener/TimeSkipListener.java
+++ paper/src/main/java/xyz/mayahive/customdaytime/paper/listener/TimeSkipListener.java
@@ -3,6 +3,7 @@ package xyz.mayahive.customdaytime.paper.listener;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ClockTimeSkipEvent;
 import org.bukkit.event.world.TimeSkipEvent;
 import xyz.mayahive.customdaytime.common.service.ConfigService;
 
@@ -19,7 +20,7 @@ public class TimeSkipListener implements Listener {
         boolean accelerationEnabled = configService.getConfigValue(Boolean.class, true, event.getWorld().key().asString(), "accelerationEnabled");
 
         if (accelerationEnabled) {
-            if (event.getSkipReason().equals(TimeSkipEvent.SkipReason.NIGHT_SKIP)) {
+            if (event.getSkipReason().equals(ClockTimeSkipEvent.SkipReason.NIGHT_SKIP)) {
                 event.setCancelled(true);
             }
         }
